3272e58502b3a0613a0e99698750f8509e111c36
SaiNushi <43940738+SaiNushi@users.noreply.github.com>
2018-10-12 23:44:00 -0400

    Update Reference.java

--- main/java/com/sainushi/mythrilmod/util/Reference.java
+++ main/java/com/sainushi/mythrilmod/util/Reference.java
@@ -3,7 +3,7 @@ package com.sainushi.mythrilmod.util;
 public class Reference {
 	public static final String MOD_ID = "mythrilmod";
 	public static final String NAME= "Mythril Mod";
-	public static final String VERSION= "1.0.3";
+	public static final String VERSION= "0.0.3a";
 	public static final String ACCEPTED_VERSIONS= "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS= "com.sainushi.mythrilmod.util.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS= "com.sainushi.mythrilmod.util.proxy.CommonProxy";
